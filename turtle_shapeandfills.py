ab53309b55f2793c8c01bdadbecb527e4090a7ce
Nisan <114586129+OKSUZN@users.noreply.github.com>
2022-09-28 17:21:20 +0200

    Add files via upload

---
+++ turtle_shapeandfills.py
@@ -0,0 +1,28 @@
+import turtle as turtle
+
+colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'black'] 
+
+# set colors for turtle
+turtle.color('red', 'blue')    # Red is de lijn kleur en Blue is de fill kleur daarom dat we de , gebruiken
+
+# set pen size
+turtle.pensize(5)   # Dit veranderd de size of de lijn van je pen tijdens het tekenen 
+
+# Fill in shape with color
+turtle.begin_fill()     # Dit command begint met het inkleuren van je cirkel
+turtle.circle(60)       # Dit maakt een cirkel , 60 = de grote van u cirkel
+turtle.end_fill()       # Dit eindigt het inkleuren van het cirkel
+
+turtle.penup()      # Penup = is als je u pen wilt oppakken zo teken je niet verder
+turtle.forward(150) 
+turtle.pendown()    # Pendown = zet de pen op het papier om verder te tekenen
+
+turtle.color('yellow', 'black')    # Geel is de lijn kleur en black is de de kleur dat word ingekleurd in het vierkant
+
+turtle.begin_fill()
+for x in range(4):      #Range is dat hij 4x het zelfde gaat uitvoeren in dit geval , 4x forward(100) en 4x right(90)
+    turtle.forward(100)
+    turtle.right(90)
+turtle.end_fill()   # Dit beëindigt het vullen van de figuur.
+
+turtle.mainloop()
